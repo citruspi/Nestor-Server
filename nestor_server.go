7485e95a9c2d772407a92d44627276d0f316fe1f
Mihir Singh <git.service@mihirsingh.com>
2014-11-02 04:01:20 -0500

    Added a route for uploading keys

--- nestor_server.go
+++ nestor_server.go
@@ -22,6 +22,7 @@ type User struct {
 	Password    string `form:"password" binding:"required"`
 	Tokens      []Token
 	Collections []Collection
+	Keys        []Key
 }
 
 type Token struct {
@@ -35,6 +36,16 @@ type Collection struct {
 	Id     int64  `json:"id"`
 	UserId int64  `json:"-"`
 	Name   string `form:"name" binding:"required" json:"name"`
+	Keys   []Key
+}
+
+type Key struct {
+	Id           int64
+	UserId       int64
+	CollectionId int64
+	Name         string `form:"name" binding:"required"`
+	Key          string `form:"key" binding:"required"`
+	Timestamp    time.Time
 }
 
 func GenerateToken() string {
@@ -325,6 +336,99 @@ func getCollectionCollection(c *gin.Context) {
 	}
 }
 
+func postKeyCollection(c *gin.Context) {
+	type Response struct {
+		Success bool   `json:"success"`
+		Error   string `json:"error,omitempty"`
+	}
+
+	var resp Response
+	var key Key
+
+	token := c.Request.FormValue("token")
+	collection := c.Request.FormValue("collection")
+
+	if token == "" {
+		resp.Success = false
+		resp.Error = "Failed to authenticate with a token."
+
+		c.JSON(403, resp)
+	} else {
+		var count int
+
+		db.Model(Token{}).Where("token = ?", token).Count(&count)
+
+		if count == 0 {
+			resp.Success = false
+			resp.Error = "Token not found."
+
+			c.JSON(404, resp)
+		} else {
+			c.BindWith(&key, binding.Form)
+
+			if key.Name == "" || key.Key == "" || collection == "" {
+				resp.Success = false
+				resp.Error = "Incomplete form submission."
+
+				c.JSON(400, resp)
+			} else {
+				var tokenRecord Token
+
+				db.Model(Token{}).Where("token = ?", token).First(&tokenRecord)
+
+				var user User
+
+				db.Model(User{}).Where("id = ?", tokenRecord.UserId).First(&user)
+
+				var collectionRecord Collection
+
+				db.Model(Collection{}).Where("id = ?", collection).First(&collectionRecord)
+
+				if collectionRecord.UserId != user.Id {
+					resp.Success = false
+					resp.Error = "You aren't authorized to access this collection."
+
+					c.JSON(403, resp)
+				} else {
+					var keys []Key
+
+					db.Model(Key{}).Where("user_id = ?", user.Id).Find(&keys)
+
+					duplicate := false
+
+					for _, item := range keys {
+						if item.Name == key.Name {
+							duplicate = true
+							break
+						}
+					}
+
+					if duplicate {
+						resp.Success = false
+						resp.Error = fmt.Sprintf("A key named '%s' already exists.", key.Name)
+
+						c.JSON(409, resp)
+					} else {
+						key.UserId = user.Id
+						key.Timestamp = time.Now()
+						db.Create(&key)
+
+						user.Keys = append(user.Keys, key)
+						collectionRecord.Keys = append(collectionRecord.Keys, key)
+
+						db.Save(&user)
+						db.Save(&collectionRecord)
+
+						resp.Success = true
+
+						c.JSON(200, resp)
+					}
+				}
+			}
+		}
+	}
+}
+
 func main() {
 	r := gin.Default()
 
@@ -338,12 +442,13 @@ func main() {
 	db.CreateTable(User{})
 	db.CreateTable(Token{})
 	db.CreateTable(Collection{})
+	db.CreateTable(Key{})
 
 	r.POST("/users/", postUserCollection)
 	r.GET("/tokens/", getTokenCollection)
 	r.POST("/tokens/", postTokenCollection)
 	r.GET("/collections/", getCollectionCollection)
 	r.POST("/collections/", postCollectionCollection)
-
+	r.POST("/keys/", postKeyCollection)
 	r.Run(":8000")
 }
